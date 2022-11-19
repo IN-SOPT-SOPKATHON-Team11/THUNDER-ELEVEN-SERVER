6fa802b1b216476a71f113ecff0c9f1899d7ac99
HoYoungChun <ho723@naver.com>
2022-11-20 02:46:18 +0900

    temp

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
 import { authService } from "../service";
 
+const login = async (req: Request, res: Response) => {
+  const { nickname, password } = req.body;
+
+  const data = await authService.getUserByNickname(+userId);
+
+  if (!data) {
+    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+  }
+  return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
+};
+
 const authController = {
+  login,
+};
 
-}
-
-export default authController
+export default authController;
--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -3,4 +3,6 @@ import { authController } from "../controller";
 
 const router: Router = Router();
 
+router.get("/login", authController.login);
+
 export default router;
